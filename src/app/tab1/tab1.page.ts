0de59224bb0b4b403b855eb430ff4a3856fa308f
Stéphane <stepxan@yahoo.es>
2022-03-08 10:56:56 +0100

    fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
+  ngOnInit() {
+    // eslint-disable-next-line no-trailing-spaces
+    
+
+    let numero = 10;
+    // eslint-disable-next-line no-debugger
+    // debugger;
+
+    numero = 20;
+
+    numero = numero + 10;
+
+    numero = 0;
+
+    console.log(numero);
+
+  }
 }
